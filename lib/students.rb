e1aef8037f1c53735de4907e5778e23e321b97bc
PARDEEP KAUR <pkaur84@aol.com>
2020-06-01 20:27:26 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,11 +9,13 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  
+  "SELECT AVERAGE(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
+  "SELECT TARDIES(gpa) FROM students;"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVERAGE(gpa) FROM 9th ;"
 end
